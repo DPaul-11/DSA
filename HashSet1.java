bdd174db5ddf9f718775decef37079cda8765e4e
DPaul-11 <125825050+DPaul-11@users.noreply.github.com>
2023-10-26 16:28:31 +0530

    Add files via upload

---
+++ HashSet1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class HashSet1 {
+    public static void main(String[] args) {
+        HashSet<String> set=new HashSet<String>();
+        set.add("Tirtha");
+        set.add("Debobrata");
+        set.add("Beast");
+        set.add("Tirtha");
+
+        System.out.println(set);
+    }
+}
